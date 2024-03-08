10eef267f7d4ce237118caaab897616fb08e8621
Guilherme Gradaschi <guilhermegradas@gmail.com>
2024-03-08 11:39:24 -0300

    dev: created serializers

---
+++ briefings/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Briefing
+
+
+class BriefingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Briefing
+        fields = '__all__'
